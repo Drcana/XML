53fa5f1f75e8920e23b49a3d36ac6d94c1d66d23
Jelena <jelenicaad@gmail.com>
2021-01-26 02:01:02 +0100

    Rename to Portal Poverenika

---
+++ spring/src/main/java/rs/ac/uns/ftn/PortalPoverenikaApplication.java
@@ -0,0 +1,12 @@
+package rs.ac.uns.ftn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalPoverenikaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalPoverenikaApplication.class, args);
+    }
+}

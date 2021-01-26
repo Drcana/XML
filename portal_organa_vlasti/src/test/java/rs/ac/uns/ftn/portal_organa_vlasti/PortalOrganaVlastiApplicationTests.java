c94954e8789bd4d719789669249c2b9055764cc5
Jelena <jelenicaad@gmail.com>
2021-01-26 15:36:31 +0100

    Initial code for portal organa vlasti

---
+++ portal_organa_vlasti/src/test/java/rs/ac/uns/ftn/portal_organa_vlasti/PortalOrganaVlastiApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.portal_organa_vlasti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalOrganaVlastiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
